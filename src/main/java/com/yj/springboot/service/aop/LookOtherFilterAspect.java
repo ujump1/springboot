cf47c596acd5393086f4e77cff2afd86ddecd4bc
余江 <jiang.yu@changhong.com>
2021-06-18 15:03:52 +0800

    aop学习

--- src/main/java/com/yj/springboot/service/aop/LookOtherFilterAspect.java
+++ src/main/java/com/yj/springboot/service/aop/LookOtherFilterAspect.java
@@ -19,7 +19,7 @@ public class LookOtherFilterAspect {
 
 
     /**
-     * 拦截findByPage,对标记的方法加上公司过滤
+     * 拦截findByPage,对标记的方法加上查看他人过滤
      *
      * @param joinPoint          切入点
      * @param search             查询条件
@@ -27,20 +27,27 @@ public class LookOtherFilterAspect {
      */
     @Before(value = "execution(* com..draw.*Service.findByPage(..)) && args(search) && @annotation(lookOtherAuthority))",
             argNames = "joinPoint,search,lookOtherAuthority")
-    public void addCorporationFilter(JoinPoint joinPoint, Search search, LookOtherAuthority lookOtherAuthority) {
+    public void addLookOtherFilter(JoinPoint joinPoint, Search search, LookOtherAuthority lookOtherAuthority) {
         String propertyName = "creatorId";
         if (StringUtils.isNotBlank(lookOtherAuthority.name())) {
             propertyName = lookOtherAuthority.name();
         }
         List<SearchFilter> searchFilterList = search.getFilters();
+        Boolean flag = false;
         if(!CollectionUtils.isEmpty(searchFilterList)){
             for(SearchFilter searchFilter: searchFilterList){
                 if(searchFilter.getFieldName().equals("lookOther")){
                     if(searchFilter.getValue().equals(false) || searchFilter.getValue().equals(Boolean.FALSE)){
-                        searchFilterList.add(new SearchFilter(propertyName, ContextUtil.getSessionUser().getUserId()));
+                        flag = true;
                     }
+                    // 注意删除操作后需要直接退出，不能继续遍历了，否则会报修改次数不一致的错。要么用迭代器遍历
+                    searchFilterList.remove(searchFilter);
+                    break;
                 }
             }
+        }
+        if(flag){
+            searchFilterList.add(new SearchFilter(propertyName, ContextUtil.getSessionUser().getUserId()));
         }
     }
 }
